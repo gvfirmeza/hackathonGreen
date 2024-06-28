4c6aa385944dfa7b800edf8799736cf62aca2bdf
Guilherme Firmeza <126484789+gvfirmeza@users.noreply.github.com>
2024-06-28 15:56:13 -0300

    Delete NELUX directory

--- NELUX/Trabalho Design Thinking - Copia.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-sensor = serial.Serial('COM3', 9600)
-total = 0
-while True:
-    i = sensor.readline().decode().rstrip()
-    if i:
-        corrente = float(i)
-        print(corrente)
