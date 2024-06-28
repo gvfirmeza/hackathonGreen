9af61151473308ea3b626b6de892cbc6b1b46e5b
gvfirmeza <gvfirmeza@gmail.com>
2024-06-28 09:23:25 -0300

    feat: on/off button

--- web/src/components/dash/Dash.js
+++ web/src/components/dash/Dash.js
@@ -1,99 +1,133 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { styled } from '@mui/system';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import BasicLineChart from "../basiclinechart/BasicLineChart";
 
 const Dashboard = styled(Box)(({ theme }) => ({
-    height: '85vh',
-    width: '70vw',
-    margin: '0 auto',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    padding: theme.spacing(4),
-    [theme.breakpoints.down('sm')]: {
-        flexDirection: 'column',
-        textAlign: 'center',
-    },
-    backgroundColor: 'rgba(0,0,0,0.1)',
+  height: "85vh",
+  width: "70vw",
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-around",
+  padding: theme.spacing(4),
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    textAlign: "center",
+  },
+  backgroundColor: "rgba(0,0,0,0.1)",
 }));
 
 const Titulo = styled(Typography)(({ theme }) => ({
-    fontWeight: 'bold',
-    fontSize: '2.5rem',
-    [theme.breakpoints.down('sm')]: {
-        fontSize: '2rem',
-    },
+  fontWeight: "bold",
+  fontSize: "2.5rem",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "2rem",
+  },
 }));
 
 const GrafBarra = styled(Box)(({ theme }) => ({
-    width: '90%',
-    height: '20em',
-    margin: '0 auto',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    padding: theme.spacing(4),
-    [theme.breakpoints.down('sm')]: {
-        flexDirection: 'column',
-    },
-    backgroundColor: 'rgba(0,0,0,0.1)',
-    borderRadius: '7px',
-    boxShadow: '0 0 10px 0 rgba(0,0,0,0.3)',
+  width: "90%",
+  height: "20em",
+  margin: "0 auto",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around",
+  padding: theme.spacing(4),
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+  backgroundColor: "rgba(0,0,0,0.1)",
+  borderRadius: "7px",
+  boxShadow: "0 0 10px 0 rgba(0,0,0,0.3)",
 }));
 
 const ContainerBaixo = styled(Box)(({ theme }) => ({
-    width: '90%',
-    height: '20em',
-    margin: '0 auto',
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    [theme.breakpoints.down('sm')]: {
-        flexDirection: 'row',
-    },
-    backgroundColor: 'blue',
-    padding: theme.spacing(4),
-    backgroundColor: 'rgba(0,0,0,0.1)',
-    borderRadius: '7px',
-    boxShadow: '0 0 10px 0 rgba(0,0,0,0.3)',
+  width: "90%",
+  height: "20em",
+  margin: "0 auto",
+  display: "flex",
+  justifyContent: "space-evenly",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "row",
+  },
+  padding: theme.spacing(4),
+  backgroundColor: "rgba(0,0,0,0.1)",
+  borderRadius: "7px",
+  boxShadow: "0 0 10px 0 rgba(0,0,0,0.3)",
 }));
 
 const BlocoTexto = styled(Box)(({ theme }) => ({
-    width: '20em',
-    textAlign: 'center',
-    padding: theme.spacing(1),
-    backgroundColor: 'red',
+  width: "18em",
+  textAlign: "center",
+  padding: theme.spacing(1),
+  backgroundColor: "#191919",
+  borderRadius: "4px",
+}));
+
+const BlocoPower = styled(Box)(({ theme }) => ({
+  width: "8em",
+  textAlign: "center",
+  padding: theme.spacing(2),
+  backgroundColor: "#191919",
+  borderRadius: "4px",
 }));
 
 const GrafPizza = styled(Box)(({ theme }) => ({
-    width: '20em',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: theme.spacing(4),
-    [theme.breakpoints.down('sm')]: {
-        flexDirection: 'column',
-        textAlign: 'center',
-    },
-    backgroundColor: 'blue',
+  width: "18em",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "4px",
+  backgroundColor: "#191919",
+  padding: theme.spacing(4),
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    textAlign: "center",
+  },
 }));
 
 const Dash = () => {
-    return (
-        <>
-            <br /><br />
-            <Dashboard>
-                <Titulo>Dashboard</Titulo>
-                <GrafBarra>Gráfico de Barra</GrafBarra>
-                <ContainerBaixo>
-                    <BlocoTexto>
-                        <Typography variant="h4">Bloco de texto</Typography>
-                        <Typography variant="body1">Texto</Typography>
-                    </BlocoTexto>
-                    <GrafPizza>Gráfico de pizza</GrafPizza>
-                </ContainerBaixo>
-            </Dashboard>
-        </>
-    );
-}
+  return (
+    <>
+      <br />
+      <br />
+      <Dashboard>
+        <Titulo sx={'margin-bottom: 15px'}>Dashboard</Titulo>
+        <GrafBarra>
+            <BasicLineChart />
+        </GrafBarra>
+        <ContainerBaixo>
+          <BlocoTexto>
+            <Typography variant="h4">Bloco de texto</Typography>
+            <Typography variant="body1">Texto</Typography>
+          </BlocoTexto>
+          <GrafPizza>Gráfico de pizza</GrafPizza>
+          <BlocoPower>
+            <Typography variant="h6">TOMADA</Typography>
+            <FormControl sx={'margin-top: 8px'}>
+              <RadioGroup name="controlled-radio-buttons-group">
+                <FormControlLabel
+                  value="ligado"
+                  control={<Radio />}
+                  label="Ligado"
+                />
+                <FormControlLabel
+                  value="desligado"
+                  control={<Radio />}
+                  label="Desligado"
+                />
+              </RadioGroup>
+            </FormControl>
+          </BlocoPower>
+        </ContainerBaixo>
+      </Dashboard>
+    </>
+  );
+};
 
 export default Dash;
