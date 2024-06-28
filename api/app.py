8d33aeca9760fc78ae2b57ac7a35d3d7bdc85e16
Ian Esteves <pk20lian@gmail.com>
2024-06-28 14:07:17 -0300

    previsao

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from datetime import datetime
+#from datetime import datetime
 import serial
 import threading
+import collections
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +15,19 @@ sensor = serial.Serial('COM3', 9600)
 total_kwh = 0
 corrente = 0
 watts = 0
+previsao_30_dias = 0
+
+historico_kwh = collections.deque(maxlen=1440)
+
+def pevisao():
+    global previsao_30_dias
+    if len(historico_kwh) == 0:
+        return jsonify({"error": "Sem dados suficientes para previsão"}), 400
+
+    media_kwh_por_tick = sum(historico_kwh) / len(historico_kwh)
+    kwh_por_dia = media_kwh_por_tick * 1440  # Assumindo que há 1440 ticks em um dia
+    previsao_30_dias = kwh_por_dia * 30
+
 
 def ler_sensor():
     global total_kwh, corrente, watts
@@ -37,8 +51,9 @@ def get_metricas():
 
     # Criar o dicionário com as métricas
     metricas = {
-        "valorTotal": round(total_kwh * 1.1, 4),  # Com acréscimo de 10%
+        "valorTotal": round(total_kwh * 0.8, 4),  # Com acréscimo de 10%
         "correnteAtual": round(watts, 2),
+        "previsao": round(previsao_30_dias* 0.8, 4)
         # "timestamp": timestamp
     }
 
@@ -57,3 +72,5 @@ def desligar():
 
 if __name__ == '__main__':
     app.run(port=8000)
+
+
