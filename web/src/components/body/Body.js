137d469c0b6ba8b7c1eee1a508186071a49f6d43
GabrielFAlves <gabrielfirmamento2@gmail.com>
2024-06-28 16:32:01 -0300

    'updatebody'

--- web/src/components/body/Body.js
+++ web/src/components/body/Body.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import { ReactTyped } from 'react-typed';
-import Planta from '../../img/undraw_environment_iaus.png'
 
 const TextoDores = () => (
-    <div>
+    <Typography variant="h4" color={'#4caf50'} width={"85%"}>
         <ReactTyped
             backSpeed={50}
             onBegin={function noRefCheck() { }}
@@ -16,24 +15,38 @@ const TextoDores = () => (
             onStart={function noRefCheck() { }}
             onStop={function noRefCheck() { }}
             strings={[
-                'transformamos tecnologia em sustentabilidade',
-                'ajudamos você a diminuir sua pegada de carbono',
+                'transformamos tecnologia em sustentabilidade!',
+                'ajudamos você a diminuir sua pegada de carbono!',
                 ''
             ]}
             typeSpeed={50}
             loop
             typedRef={function noRefCheck() { }}
         />
-    </div>
+    </Typography>
 );
 
 const Section = styled(Box)(({ theme }) => ({
+    height: '85vh',
+    width: '85%',
+    margin: '0 auto',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: theme.spacing(4),
+    [theme.breakpoints.down('sm')]: {
+        flexDirection: 'column',
+        textAlign: 'center',
+    },
+}));
+
+const Section2 = styled(Box)(({ theme }) => ({
     height: '85vh',
     width: '70vw',
     margin: '0 auto',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     padding: theme.spacing(4),
     [theme.breakpoints.down('sm')]: {
         flexDirection: 'column',
@@ -47,15 +60,23 @@ const TextBox = styled(Box)(({ theme }) => ({
 
 
 const Image = styled('img')(({ theme }) => ({
-    maxWidth: '45%',
+    maxWidth: '50%',
     borderRadius: '7px',
-    margin: '10px',
+    margin: '15px',
     opacity: 0.8,
     boxShadow: '0 0 10px 0 rgba(0,0,0,0.8)',
     height: 'auto',
     [theme.breakpoints.down('sm')]: {
         marginTop: theme.spacing(2),
     },
+}));
+
+const Lista = styled('ul')(({ theme }) => ({
+    listStyleType: 'disc',
+    paddingLeft: '20px',
+    '& li::marker': {
+        color: '#4caf50',
+    }
 }));
 
 const FullWidthBox = styled(Box)({
@@ -65,6 +86,31 @@ const FullWidthBox = styled(Box)({
 const Body = () => {
     return (
         <>
+            <FullWidthBox>
+                <Section>
+                    <TextBox sx={"width: 70%"}>
+                        <Typography variant="h3">Venha entender como</Typography>
+                        <TextoDores />
+                    </TextBox>
+                    <Image src="https://miro.medium.com/v2/resize:fit:1400/1*YSVUSwyLaGqZANAf4y6p8g.png" alt="Descrição da Imagem"/>
+                </Section>
+            </FullWidthBox>
+
+            <FullWidthBox sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <Section2>
+                    <Image src="https://portaldobiogas.com/wp-content/uploads/2023/12/PBG-ART-ENERGIAS-1024x576.jpg.webp" alt="Descrição da Imagem" />
+                    <TextBox>
+                        <Typography variant="h3">Nossa Missão</Typography>
+                        <Lista>
+                            <li>Reduzir a Pegada de Carbono no mundo atual</li>
+                            <li>Oferecer praticidade ao usuário</li>
+                            <li>Diminuir custos desnecessários</li>
+                            <li>Compromisso com o Meio Ambiente</li>
+                        </Lista>
+                    </TextBox>
+                </Section2>
+            </FullWidthBox>
+
             <FullWidthBox>
                 <Section>
                     <TextBox>
@@ -78,32 +124,7 @@ const Body = () => {
                         </Typography>
 
                     </TextBox>
-                    <Image src="https://www.amigoscode.com/assets/thumbnails/courses/mongodb.webp" alt="Descrição da Imagem" />
-                </Section>
-            </FullWidthBox>
-
-            <FullWidthBox sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                <Section>
-                    <Image src={Planta} alt="Descrição da Imagem" />
-                    <TextBox>
-                        <Typography variant="h3">Nossa Missão</Typography>
-                        <ul>
-                            <li>Reduzir a Pegada de Carbono no mundo atual</li>
-                            <li>Oferecer praticidade ao usuário</li>
-                            <li>Diminuir custos desnecessários</li>
-                            <li>Compromisso com o Meio Ambiente</li>
-                        </ul>
-                    </TextBox>
-                </Section>
-            </FullWidthBox>
-
-            <FullWidthBox>
-                <Section>
-                    <TextBox>
-                        <Typography variant="h3">Venha entender como</Typography>
-                        <TextoDores />
-                    </TextBox>
-                    <Image src="https://img-c.udemycdn.com/course/480x270/3764174_3ca2_2.jpg" alt="Descrição da Imagem" />
+                    <Image src="https://tratamentodeagua.com.br/wp-content/uploads/2023/03/Fontes-de-energia-renovaveis.jpg" alt="Descrição da Imagem" />
                 </Section>
             </FullWidthBox>
         </>
